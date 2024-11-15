74992bbfd9fcc7d2c1ab832c46b89269009141e4
maiducbinh <binhmai2205@gmail.com>
2024-11-15 14:32:37 +0700

    NGUYÊN TỐ CÙNG NHAU

---
+++ BienVaKieuDuLieuDonGian/PY01030.py
@@ -0,0 +1,12 @@
+from math import *
+
+n, k = map(int, input().split())
+a = 10 ** (k - 1)
+b = 10 ** k
+cnt = 0
+for i in range(a, b):
+    if gcd(i, n) == 1:
+        print(i, end = " ")
+        cnt += 1
+        if cnt % 10 == 0:
+            print()
