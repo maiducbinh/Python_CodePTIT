01510dc60191ee8c0c1d131bc0be5a725070d71c
maiducbinh <binhmai2205@gmail.com>
2024-11-25 15:04:34 +0700

    ĐẾM CÁC SỐ CÓ HAI CHỮ SỐ

---
+++ DanhSach/PY02034.py
@@ -0,0 +1,11 @@
+s = input()
+a = [int(s[i] + s[i + 1]) for i in range(0, len(s) - 1, 2)]
+mp = {}
+for i in a:
+    if i not in mp:
+        mp[i] = 1
+    else:
+        mp[i] += 1
+for i in mp:
+    print(i, mp[i])
+    
